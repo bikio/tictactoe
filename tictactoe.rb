758bb7ab48f1f2ec4d850724c4a1fe88491b469a
Kelli and Alex <kelliandalex@DPL-1s-iMac.local>
2014-02-11 11:49:27 -0800

    first edit

---
+++ tictactoe.rb
@@ -0,0 +1,72 @@
+# class Board
+
+# 	def initialize(x,o)
+# 		@grid = [["x", "o", nil], [nil, nil, nil], [nil, nil, nil]]
+# 	end
+
+# 	def show
+# 	puts " #{@grid[0][0]} |  #{@grid[0][1]}  |  #{@grid[0][2]} "
+
+# 	puts "_____________________________________________________"
+
+# 	puts " #{@grid[1][0]} |  #{@grid[1][1]} |  #{@grid[1][2} "
+
+# 	puts "______________________________________________________"
+
+# 	puts " #{@grid[2][0]}  |  #{@grid[2][1}  |  #{@grid[2][2]} "
+
+		
+# 	end
+
+# end
+
+class Player
+	
+	attr_accessor :name 
+	def to_s
+		"unknown player type"
+
+	end
+
+end
+
+class PlayerX < Player
+	def to_s
+		"x"
+	end
+end
+
+class PlayerO < Player
+	def to_s
+		"o"
+	end
+end
+
+class Game
+	def initialize
+	@board = Board.new
+	@player1 = PlayerX.new
+	@player2 = PlayerO.new
+	
+	puts "What is your player1 name?"
+	player1_name = gets.chomp
+	puts "Player 1 set name to: #{player1_name}" 
+	puts "What is player2 name?"
+	player2_name = gets.chomp
+	puts "Player 2 set name to: #{player2_name}"
+	
+	player1.name=player1_name
+	player2.name=player2_name
+	
+	@game_in_progress = true
+
+	end
+
+	def game_loop
+		while @game_in_progress do
+			puts "Make a move, #{player1}!"
+
+		end
+	end
+
+end
